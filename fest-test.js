2a080b4624ad8a9688233823f59dffc87b186156
Lebedev Konstantin <k.lebedev@corp.mail.ru>
2011-11-11 12:30:49 +0300

    stream = false

--- fest-test.js
+++ fest-test.js
@@ -1,10 +1,11 @@
 var Tpl		= require('./lib/Fest');
 var http	= require('http');
+var data	= JSON.parse(require('fs').readFileSync('checknew.json') + '').data;
 
 console.time('compile');
 
-var page = (new Tpl('./tpl/messagelist.xml'))
-	.set( JSON.parse(require('fs').readFileSync('checknew.json') + '').data )
+var page = (new Tpl('./tpl/messagelist.xml', { stream: false }))
+	.set( data )
 	.on('start', function (){
 //		console.timeEnd('compile');
 //		console.time('stream');
@@ -21,13 +22,11 @@ http.createServer(function (req, res){
 	if (req.url == '/favicon.ico') return;
 	res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
 	console.time('stream');
+
 	page
-		.on('data', function (chunk){
-			res.write(chunk);
-		})
-		.on('end', function (){
-			res.end('');
+		.on('end', function (result){
 			console.timeEnd('stream');
+			res.end(result);
 		})
 		.fetch()
 	;
