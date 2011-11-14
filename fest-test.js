cdc40ceca705175afc978718604bb042179a7fcd
Lebedev Konstantin <k.lebedev@corp.mail.ru>
2011-11-14 18:44:02 +0300

    * x1000

--- fest-test.js
+++ fest-test.js
@@ -7,28 +7,39 @@ console.time('compile');
 var page = (new Tpl('./tpl/messagelist.xml', { stream: false }))
 	.set( data )
 	.preload()
-	.on('start', function (){
-//		console.timeEnd('compile');
-//		console.time('stream');
-	})
-	.on('data', function (str){
-//		console.log(str);
-	})
-	.on('end', function (){
-		console.timeEnd('stream');
-	})
 ;
 
-http.createServer(function (req, res){
-	if (req.url == '/favicon.ico') return;
-	res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-	console.time('stream');
 
-	page
-		.on('end', function (result){
-			console.timeEnd('stream');
-			res.end(result);
-		})
-		.fetch()
-	;
-}).listen(8082);
+if( false ){
+	for( var i = 0; i < 10; i++ ){
+		console.time('stream');
+		page
+			.on('end', function (){
+				console.timeEnd('stream');
+			})
+			.fetch()
+		;
+	}
+} else {
+	http.createServer(function (req, res){
+		if (req.url == '/favicon.ico') return;
+		res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+		console.time('all');
+		var html = '';
+
+		for( var i = 0; i < 1000; i++ ){
+			console.time('stream');
+			page
+				.on('data', function (chunk){ res.write(chunk); })
+				.on('end', function (result){
+//					console.timeEnd('stream');
+					html = result;
+				})
+				.fetch()
+			;
+		}
+
+		console.timeEnd('all');
+		res.end(html);
+	}).listen(8082);
+}
