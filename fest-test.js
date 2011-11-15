9655095821c50565582d28964b555fd74eaeea2d
Lebedev Konstantin <k.lebedev@corp.mail.ru>
2011-11-15 11:58:56 +0300

    * x1000 example

--- fest-test.js
+++ fest-test.js
@@ -1,21 +1,22 @@
 var Tpl		= require('./lib/Fest');
 var http	= require('http');
-var data	= JSON.parse(require('fs').readFileSync('checknew.json') + '').data;
+var page	= (new Tpl('./tpl/messagelist.xml', { stream: false })).preload();
 
-console.time('compile');
 
-var page = (new Tpl('./tpl/messagelist.xml', { stream: false }))
-	.set( data )
-	.preload()
-;
 
 
 if( false ){
-	for( var i = 0; i < 10; i++ ){
+	for( var i = 0; i < 1; i++ ){
+		var data	= JSON.parse(require('fs').readFileSync('checknew.json') + '').data;
 		console.time('stream');
 		page
-			.on('end', function (){
-				console.timeEnd('stream');
+			.set(data)
+			.on('data', function (chunk){
+//				console.log(chunk);
+			})
+			.on('end', function (res){
+//				console.log(res);
+//				console.timeEnd('stream');
 			})
 			.fetch()
 		;
@@ -25,20 +26,25 @@ if( false ){
 		if (req.url == '/favicon.ico') return;
 		res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
 		console.time('all');
-		var html = '';
+		var html = '', total = 0, start;
 
 		for( var i = 0; i < 1000; i++ ){
+			var data	= JSON.parse(require('fs').readFileSync('checknew.json') + '').data;
 			console.time('stream');
+			start = (new Date).getTime();
 			page
+				.set( data )
 				.on('data', function (chunk){ res.write(chunk); })
 				.on('end', function (result){
 //					console.timeEnd('stream');
 					html = result;
+					total += (new Date).getTime() - start;
 				})
 				.fetch()
 			;
 		}
 
+		console.log('total:', total+'ms');
 		console.timeEnd('all');
 		res.end(html);
 	}).listen(8082);
