7a6110ef34fa1d0b6ea5fbd0ec0beda26d957d32
RubaXa <ibnRubaXa@gmail.com>
2012-09-06 22:43:43 +0400

    + lebowski benchmark

---
+++ benchmark/README.md
@@ -0,0 +1,47 @@
+# Benchmark
+
+Исходный тест: http://ctpp.havoc.ru/template_benchmarks.html
+
+----------------------------
+
+## Node v0.6.11
+Memory     8 GB 1333 MHz DDR3
+Processor  2,4 GHz Intel Core i7
+
+
+     TOP SCORE
+---------------------
+ 1. xtpl
+ 2. TSN    +38.58%
+ 3. fest   +51.15%
+
+
+
+TSN
+---------------------
+fps:   26246.7192
+time:  0.0381ms
+total: 0.381s
+---------------------
+content:  19.095 KB
+template: 7.369 KB
+
+
+fest
+---------------------
+fps:   20876.8267
+time:  0.0479ms
+total: 0.479s
+---------------------
+content:  8.210 KB
+template: 7.261 KB
+
+
+xtpl
+---------------------
+fps:   42735.0427
+time:  0.0234ms
+total: 0.234s
+---------------------
+content:  8.211 KB
+template: 3.123 KB
---
+++ benchmark/lebowski.js
@@ -0,0 +1,98 @@
+var data = JSON.parse(require('fs').readFileSync('./lebowski.json'));
+var TestSuites = [];
+
+TestSuites.run = function (){
+	TestSuites.forEach(function (unit){
+		unit.setup();
+
+		var
+			  time = (new Date).getTime()
+			, max = 10000
+			, i = max
+		;
+
+		while( i-- ){
+			unit.test();
+		}
+
+		unit.total	= time = (new Date).getTime() - time;
+		unit.time	= (time / max);
+
+		console.log([
+			  unit.name
+			, '---------------------'
+			, 'fps:   ' + (max / time * 1000).toFixed(4)
+			, 'time:  ' + unit.time + 'ms'
+			, 'total: ' + (time / 1000) + 's'
+			, '---------------------'
+			, 'content:  '+ (unit.result.length/1024).toFixed(3) + ' KB'
+			, 'template: '+ (unit.template.toString().length/1024).toFixed(3) +' KB'
+			, ''
+		].join('\n'));
+	});
+
+
+	TestSuites.sort(function (a, b){
+		return	a.total - b.total;
+	});
+
+
+	console.log('\n     TOP SCORE');
+	console.log('---------------------');
+	TestSuites.forEach(function (unit, i){
+		console.log(
+			  i ? '' : '\033[0;32m'
+			, (i+1)+'.'
+			, unit.name
+			, (new Array(Math.max(0, 6-unit.name.length))).join(' ')
+			, i ? '+' + ((1-TestSuites[0].time/unit.time)*100).toFixed(2) + '%' : ''
+			, ' \033[0m'
+		);
+	});
+};
+
+
+/**
+ *           ~~    TESTS   ~~~
+ */
+
+TestSuites.push(
+	{
+		name: 'TSN',
+		setup: function (){
+			var TSN = require('../../TSN');
+
+			this.template	= TSN.load('tsn.lebowski.xml', null, {
+				indent:4,
+				templateRoot:__dirname
+			});
+		},
+		test: function (){ this.result = this.template.call(data); }
+	},
+
+	{
+		name: 'fest',
+		setup: function (){
+			var fest = require('../../fest.mail.ru/lib/fest'), compiled = fest.compile('fest.lebowski.xml', {beatify:false});
+			this.template = (new Function('return ' + compiled))();
+		},
+		test: function (){ this.result = this.template(data); }
+	},
+
+
+	{
+		name: 'xtpl',
+		setup: function (){
+			var _this = this;
+			var xtpl = require('../lib/AsyncTpl').engine('XML');
+
+			this.xtpl = new xtpl('xtpl.lebowski.xml', { async: false });
+			this.template = this.xtpl.compile(); // Force compile
+			this.render = function (res){ _this.result = res; };
+		},
+		test: function (){ this.xtpl.fetch(data, this.render); }
+	}
+);
+
+
+TestSuites.run();
